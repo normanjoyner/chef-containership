ece36f6bc3a7761930d5f3d8eecb5a537a6b2b87
Norman Joyner <norman.joyner@gmail.com>
2016-01-19 12:24:36 -0500

    Bump version to 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'developers@containership.io'
 license          'All rights reserved'
 description      'Chef cookbook to set up and configure ContainerShip'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version			'0.0.0'
+version			'0.1.0'
 
 depends 'apt', '~> 2.0'
 depends 'docker', '~> 2.0'
