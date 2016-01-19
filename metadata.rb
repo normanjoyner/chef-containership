d78bd312239f0fba692535347fd7a9433c9a0112
Norman Joyner <norman.joyner@gmail.com>
2016-01-19 13:27:10 -0500

    adds source and issues urls

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description      'Chef cookbook to set up and configure ContainerShip'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version			'0.1.0'
 
+source_url "https://github.com/containership/chef-containership"
+issues_url "https://github.com/containership/chef-containership/issues"
+
 depends 'apt', '~> 2.0'
 depends 'docker', '~> 2.0'
 depends 'nodejs', '~> 2.0'
